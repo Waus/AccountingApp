f01f5c7d4757461463e7f5cfacaaa4eea4d6720e
waus <kamil.muranski.official@gmail.com>
2020-06-19 19:54:33 +0200

    Generator JPK - Testy jednostkowe

--- AccountingApp.Model/Model.Designer.cs
+++ AccountingApp.Model/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Git\JPK generator\AccountingApp.Model\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Git\AccountingApp\AccountingApp.Model\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
